a1d12859fded72e1f9414481178d3d202d6db93a
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-07-27 22:44:29 +0200

    Init repository

---
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/BeyondfootstepsApplication.java
@@ -0,0 +1,13 @@
+package com.beyondfootsteps.beyondfootsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondfootstepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondfootstepsApplication.class, args);
+	}
+
+}
