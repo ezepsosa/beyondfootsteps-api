a04512aab66d7941f6e40c151a8777d793bb0134
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-07-28 16:53:18 +0200

    Add dashboard summary model

---
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/models/DashboardSummary.java
@@ -0,0 +1,58 @@
+package com.beyondfootsteps.beyondfootsteps.models;
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "dashboard_summary_kpi")
+public class DashboardSummary {
+
+    private int year;
+
+    private String countryIso;
+
+    private int totalApplied;
+
+    private float appliedPer100k;
+
+    private float acceptanceRate;
+
+    private int internalDisplacementTotal;
+
+    private float displacementRatePer100k;
+
+    private int idpReturnees;
+
+    private int refugeesReturnees;
+
+    private int naturalizationsTotal;
+
+    private float naturalizationChange;
+
+    private int resettlementRequests;
+
+    private int resettlementDepartures;
+
+    private int resettlementSubmissions;
+
+    private int resettlementNeeds;
+
+    private float resettlementGaps;
+
+    private float coverageRate;
+
+    private float requestVsNeedsRatio;
+
+    private float submissionsEfficiency;
+
+    private float realizationRate;
+
+}
