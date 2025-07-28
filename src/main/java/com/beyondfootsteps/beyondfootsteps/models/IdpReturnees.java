5a9e7349a30d73ebb0259e8ba71abfcbd73f31ef
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-07-28 17:11:19 +0200

    Add idp returnees models

---
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/models/IdpReturnees.java
@@ -0,0 +1,39 @@
+package com.beyondfootsteps.beyondfootsteps.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "idp_returnees_kpi")
+public class IdpReturnees {
+
+    @Id
+    private String idIdpreturnees;
+
+    private int year;
+
+    private String countryOfOriginIso;
+
+    private String countryOfOrigin;
+
+    @Column(name="idp_returnees")
+    private int idpReturneesNumber;
+
+    private int refugeesReturnees;
+
+    private LocalDate byDate;
+
+
+}
