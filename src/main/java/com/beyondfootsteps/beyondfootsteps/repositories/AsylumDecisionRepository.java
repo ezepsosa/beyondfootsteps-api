0d78810356c507ebb5f407261a23f342b9062fe4
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-07-28 21:54:40 +0200

    Add repository layer

---
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/repositories/AsylumDecisionRepository.java
@@ -0,0 +1,11 @@
+package com.beyondfootsteps.beyondfootsteps.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.beyondfootsteps.beyondfootsteps.models.AsylumDecision;
+
+@Repository
+public interface AsylumDecisionRepository extends JpaRepository<AsylumDecision, String>{
+
+}
