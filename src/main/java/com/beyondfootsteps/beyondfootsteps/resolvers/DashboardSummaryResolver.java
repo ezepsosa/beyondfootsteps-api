fc9eb9b2cb0f3ea40fd0412eb4e3475b0b8ec587
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-04 16:33:55 +0200

    Add dashboardSummary fields and methods to filter by year

--- src/main/java/com/beyondfootsteps/beyondfootsteps/resolvers/DashboardSummaryResolver.java
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/resolvers/DashboardSummaryResolver.java
@@ -2,6 +2,7 @@ package com.beyondfootsteps.beyondfootsteps.resolvers;
 
 import java.util.List;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -19,6 +20,11 @@ public class DashboardSummaryResolver {
     @QueryMapping(name = "dashboardSummaries")
     public List<DashboardSummary> findAll() {
         return dashboardSummaryService.findAll();
+    }
+
+    @QueryMapping(name = "dashboardSummariesByYear")
+    public List<DashboardSummary> findByYear(@Argument int year){
+        return dashboardSummaryService.findByYear(year);
     }
 
 }
