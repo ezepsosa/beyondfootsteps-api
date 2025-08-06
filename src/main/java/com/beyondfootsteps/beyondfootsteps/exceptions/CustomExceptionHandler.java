2cba56fea486dd7e18909378ec336c8347c16e0b
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-06 14:27:04 +0200

    Add asylum requests operations. Add exception handling.

---
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.beyondfootsteps.beyondfootsteps.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("message", ex.getMessage());
+        errorDetails.put("details", request.getDescription(false));
+        errorDetails.put("status", HttpStatus.BAD_REQUEST.value());
+        
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+}
--- src/main/java/com/beyondfootsteps/beyondfootsteps/services/AsylumRequestService.java
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/services/AsylumRequestService.java
@@ -18,4 +18,8 @@ public class AsylumRequestService {
     public List<AsylumRequest> findAll() {
         return asylumRequestRepository.findAll();
     }
+
+    public List<AsylumRequest> findByYearAndCountry(int year, String countryOfOriginIso, String countryOfAsylumIso) {
+        return asylumRequestRepository.findByYearAndCountries(year, countryOfOriginIso, countryOfAsylumIso);
+    }
 }
