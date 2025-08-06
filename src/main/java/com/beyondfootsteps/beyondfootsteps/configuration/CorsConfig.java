8053669388fe4e41467a635504b2254838c2397f
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-06 16:02:02 +0200

    Add docker configuration

--- src/main/java/com/beyondfootsteps/beyondfootsteps/configuration/CorsConfig.java
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/configuration/CorsConfig.java
@@ -2,21 +2,26 @@ package com.beyondfootsteps.beyondfootsteps.configuration;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+
 @Configuration
 public class CorsConfig {
 
+    @Value("${ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Bean
-    public CorsFilter corsFilter(){
+    public CorsFilter corsFilter() {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
         config.setAllowedMethods(List.of("POST"));
         config.setAllowedHeaders(List.of("*"));
 
