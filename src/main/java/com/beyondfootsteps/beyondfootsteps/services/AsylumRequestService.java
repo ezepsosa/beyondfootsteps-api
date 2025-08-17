3d8102e86558782d05088ff50d1ad37c476ac1d3
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-17 21:39:14 +0200

    Enhance AsylumRequestService with logging and improve Javadoc comments for clarity

--- src/main/java/com/beyondfootsteps/beyondfootsteps/services/AsylumRequestService.java
+++ src/main/java/com/beyondfootsteps/beyondfootsteps/services/AsylumRequestService.java
@@ -1,6 +1,7 @@
 package com.beyondfootsteps.beyondfootsteps.services;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
 
@@ -9,16 +10,36 @@ import com.beyondfootsteps.beyondfootsteps.repositories.AsylumRequestRepository;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ *
+ * Service for managing asylum requests
+ */
 @Service
 @RequiredArgsConstructor
 public class AsylumRequestService {
 
     private final AsylumRequestRepository asylumRequestRepository;
 
+    private final Logger logger = Logger.getLogger(AsylumRequestService.class.getName());
+
+    /**
+     * Retrieves all asylum requests from the repository.
+     *
+     * @return a list of asylum requests
+     */
     public List<AsylumRequest> findAll() {
         return asylumRequestRepository.findAll();
     }
 
+    /**
+     * Retrieves asylum requests filtered by year and country of asylum or
+     * origin
+     *
+     * @param year the year of the asylum request
+     * @param countryOfOriginIso the ISO code of the country of origin
+     * @param countryOfAsylumIso the ISO code of the country of asylum
+     * @return a list of asylum decisions matching the criteria
+     */
     public List<AsylumRequest> findByYearAndCountry(int year, String countryOfOriginIso, String countryOfAsylumIso) {
         return asylumRequestRepository.findByYearAndCountries(year, countryOfOriginIso, countryOfAsylumIso);
     }
