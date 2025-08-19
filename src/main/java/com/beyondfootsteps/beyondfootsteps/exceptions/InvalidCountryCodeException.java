559107d98989118e67e532b0a4f14bcd53753af8
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-19 16:05:03 +0200

    Remove InvalidCountryCodeException class as it is no longer needed

--- src/main/java/com/beyondfootsteps/beyondfootsteps/exceptions/InvalidCountryCodeException.java
+++
@@ -1,9 +0,0 @@
-package com.beyondfootsteps.beyondfootsteps.exceptions;
-
-public class InvalidCountryCodeException extends RuntimeException {
-
-    public InvalidCountryCodeException() {
-        super("Invalid country code.");
-    }
-
-}
